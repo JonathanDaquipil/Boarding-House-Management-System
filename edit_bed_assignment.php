c76f3528690b27de0b6b76a857503ea75f568880
Rosemarie Calunod <162532148+calunod143@users.noreply.github.com>
2024-06-12 02:39:29 -0700

    Update edit_bed_assignment.php

--- edit_bed_assignment.php
+++ edit_bed_assignment.php
@@ -14,14 +14,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
               WHERE assignment_id = '$assignmentId'";
 
     if ($conn->query($query) === TRUE) {
-        // Redirect to bed management page or any other desired page after successful update
+       
         header("Location: bedassignment.php");
         exit();
     } else {
         echo "Error updating record: " . $conn->error;
     }
 
-    // Close database connection
     $conn->close();
 }
 ?>
