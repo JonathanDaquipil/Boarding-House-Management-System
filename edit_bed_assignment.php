990e9a0d5de753582f2b63fbfbda01e992840d3f
Rosemarie Calunod <162532148+calunod143@users.noreply.github.com>
2024-06-12 02:39:12 -0700

    Update edit_bed_assignment.php

--- edit_bed_assignment.php
+++ edit_bed_assignment.php
@@ -3,14 +3,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
    
     require 'config.php';
 
-    // Retrieve data from the form
     $assignmentId = $_POST['assignment_id'];
     $roomNo = $_POST['room_no'];
     $bedNo = $_POST['bed_no'];
     $dateStart = $_POST['date_start'];
     $dueDate = $_POST['due_date'];
 
-    // Perform update query
     $query = "UPDATE bed_assignment_table 
               SET room_no = '$roomNo', bed_no = '$bedNo', date_start = '$dateStart', due_date = '$dueDate'
               WHERE assignment_id = '$assignmentId'";
