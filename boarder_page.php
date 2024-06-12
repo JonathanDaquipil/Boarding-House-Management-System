fdf3e54dcbd54298ffdfd1519d17fe8d5388edac
JonathanDaquipil <162531541+JonathanDaquipil@users.noreply.github.com>
2024-06-11 22:25:37 -0700

    Update boarder_page.php

---
+++ boarder_page.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+    <link rel="stylesheet" href="adminpage.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+</head>
+<body>
+    
+    <div class="list" id="adminpage">
+       
+    <img src="logo1.png"  class="pic">
+
+
+    <ul>
+  <pre>
+  <li><a href="boarder_page.php"><i class="fa-solid fa-house"></i>  DashBoard</a></li>   
+  <li><a href="boarder_notice.php"><i class="fa-solid fa-people-line"$nbsp;></i>  Notice</a></li>
+  <li><a href="boarder_pop.php"><i class="fa-solid fa-house-lock"></i>  Proof of Payment</a></li>
+  <li><a href="boarder_ph.php"><i class="fa-solid fa-bed"></i>  Payment History</a></li>
+ 
+  
+</ul>
+</pre>
+</div>
+
+
+
+<div class="formtitle" id="adminpage">
+        <h1>Dashboard</h1>
+        <div class="dashboard">
+        <div class="card balance-card">
+                <span>Balance: 10,000 </span>
+            </div>
+            <div class="card paymenthistory-card">
+                <span>Total Rooms: 20,000 </span>
+            </div>
+         
+        </div>
+    </div>
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+
+
+</body>
+</html>
