4deee6d9955d4d3afa55d6c1041913b5153d084e
Rosemarie Calunod <162532148+calunod143@users.noreply.github.com>
2024-06-12 02:34:50 -0700

    Update delete_payment_proof.php

--- delete_payment_proof.php
+++ delete_payment_proof.php
@@ -4,7 +4,7 @@ include 'config.php';
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $id = $_POST['payment_id'];
 
-    // Delete the record from boarder_payments_proof table
+    
     $sql = "DELETE FROM payments WHERE payment_id = ?";
     $stmt = $conn->prepare($sql);
     $stmt->bind_param("i", $payment_id);
@@ -14,8 +14,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         echo 'error';
     }
 
-    // Optionally, you might want to delete the corresponding record from the payments table
-    // Uncomment the following lines if needed
+  
     // $sql2 = "DELETE FROM payments WHERE payment_id = ?";
     // $stmt2 = $conn->prepare($sql2);
     // $stmt2->bind_param("i", $id);
