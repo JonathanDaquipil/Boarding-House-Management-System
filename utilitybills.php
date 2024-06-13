66f436588d184e992bc519f5e78cd127e6e3627d
JonathanDaquipil <162531541+JonathanDaquipil@users.noreply.github.com>
2024-06-12 21:08:36 -0700

    Update utilitybills.php

--- utilitybills.php
+++ utilitybills.php
@@ -170,7 +170,7 @@ if (!$result) {
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
     <script>
-        // Pass data to the edit modal
+
         $('#editBillModal').on('show.bs.modal', function (event) {
             var button = $(event.relatedTarget);
             var billId = button.data('bill_id');
